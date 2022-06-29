8d24c48d1ed4f55692ab3bd0cdd9130b497e1a6c
siuloong <syau0003@student.monash.edu>
2022-06-30 00:36:22 +0800

    Finished portfolio section allowing users to navigate to github

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,71 @@
 import React from "react";
 import "./portfolio.css";
+import BTA from "../../assets/attendance.png";
+import IOTA from "../../assets/heartfailure.jpg";
+import DM from "../../assets/darkmode.jpeg";
+import PF from "../../assets/portfolio.png";
+import WEB from "../../assets/webby.png";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio_item">
+          <div className="portfolio__item-image">
+            <img src={BTA} alt="" />
+          </div>
+          <h3>
+            Year Long Project - Bedside teaching attendance system (Mobile App)
+          </h3>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio__item-image">
+            <img src={IOTA} alt="" />
+          </div>
+          <h3>Final Year Project - Offline Patient system (Mobile App) </h3>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio__item-image">
+            <img src={DM} alt="" />
+          </div>
+          <h3>
+            Research Project - Automated Darkmode Generation (Machine Learning)
+          </h3>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio__item-image">
+            <img src={WEB} alt="" />
+          </div>
+          <h3>Simple Autentication system (Mobile App)</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/yausiuloong/WebbyInterview"
+              className="btn"
+              target="_blank">
+              GitHub
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio__item-image">
+            <img src={PF} alt="" />
+          </div>
+          <h3>Personal Portfolio Website (Web App)</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/yausiuloong/react-portfolio-website"
+              className="btn"
+              target="_blank">
+              GitHub
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
